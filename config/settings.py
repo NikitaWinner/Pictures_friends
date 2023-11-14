7af7e4f03b323cabea70c9aa64b56438bb03b3f5
Nikita <kyle.winner@gmail.com>
2023-11-14 05:39:45 +0300

    add reset password

--- config/settings.py
+++ config/settings.py
@@ -112,6 +112,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
